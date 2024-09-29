ce9783ed58d056a39784c5f72db9cab348217003
Sujal Raval <88503112+s123u@users.noreply.github.com>
2024-09-29 23:52:42 +0530

    initial commit

---
+++ 3_printing_text.py
@@ -0,0 +1,4 @@
+print("my favorite quote is \"to be or not to be \"")
+print('my favorite quote is "to be or not to be"')
+print("""my favorite quote is "to be or not to be " """)
+print('my favorite quote is "to be or not to be"')
---
+++ 4_comments.py
@@ -0,0 +1,9 @@
+# this is a comment. 
+# Anything after the # is ignored by python.
+
+
+print("I belong here")
+# print("why am i here")
+# print("get me out of here")
+print("I also belong here")
+print("Me too")
